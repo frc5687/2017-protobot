d42193c0fd4df2ad9c3d00de9fd2e71b15c34e88
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-01-12 23:37:07 -0500

    Initial framework for the steamworks protobot.

---
+++ src/main/java/org/frc5687/steamworks/protobot/Constants.java
@@ -0,0 +1,7 @@
+package org.frc5687.steamworks.protobot;
+
+/**
+ * Created by Ben Bernard on 1/12/2017.
+ */
+public class Constants {
+}
---
+++ src/main/java/org/frc5687/steamworks/protobot/Robot.java
@@ -0,0 +1,10 @@
+package org.frc5687.steamworks.protobot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Created by Ben Bernard on 1/12/2017.
+ */
+public class Robot extends IterativeRobot {
+
+}
