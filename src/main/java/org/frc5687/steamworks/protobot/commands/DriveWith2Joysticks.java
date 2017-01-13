fd646ce6dbc7216c69c4ccedfad921c06abc2e74
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-01-13 09:29:14 -0500

    Adding DriveTrain subsystem and DriveWith2Joysticks command.

---
+++ src/main/java/org/frc5687/steamworks/protobot/commands/DriveWith2Joysticks.java
@@ -0,0 +1,13 @@
+package org.frc5687.steamworks.protobot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Created by Ben Bernard on 1/13/2017.
+ */
+public class DriveWith2Joysticks extends Command {
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/main/java/org/frc5687/steamworks/protobot/subsystems/DriveTrain.java
@@ -0,0 +1,14 @@
+package org.frc5687.steamworks.protobot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by Ben Bernard on 1/13/2017.
+ */
+public class DriveTrain extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
