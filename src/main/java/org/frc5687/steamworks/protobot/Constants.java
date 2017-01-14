44b944c535c9bd03ea773f515530a4cd11891a23
Caleb Marston <caleb.marston@baxter-academy.org>
2017-01-14 10:53:46 -0500

    Adds OI chasis system, helpers class, and constants class

--- src/main/java/org/frc5687/steamworks/protobot/Constants.java
+++ src/main/java/org/frc5687/steamworks/protobot/Constants.java
@@ -4,4 +4,23 @@ package org.frc5687.steamworks.protobot;
  * Created by Ben Bernard on 1/12/2017.
  */
 public class Constants {
+
+    public class Deadbands {
+        /**
+         * Deadband threshold for drive joysticks
+         */
+        public static final double DRIVE_STICK = 0.1;
+    }
+
+    public class Calibration {
+        /***
+         * Controls the sensitivity algorithm.
+         * 0 results in a linear control-to-speed relationship, while 1 results in cubed.
+         *
+         * NEVER SET ABOVE 1 OR BELOW 0
+         */
+        public static final double SENSITIVITY_FACTOR = 0.5;
+    }
+
+
 }
