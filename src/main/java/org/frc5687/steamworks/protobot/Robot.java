62e0cc58a16b6d7cf318b2178a4c1705a13c3012
Ian Farm <ian.farm@baxter-academy.org>
2017-01-14 14:49:34 -0500

    Added DriveWith2Joysticks command and Robot class based on chassibot

--- src/main/java/org/frc5687/steamworks/protobot/Robot.java
+++ src/main/java/org/frc5687/steamworks/protobot/Robot.java
@@ -1,10 +1,30 @@
 package org.frc5687.steamworks.protobot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.frc5687.steamworks.protobot.subsystems.DriveTrain;
 
 /**
  * Created by Ben Bernard on 1/12/2017.
  */
 public class Robot extends IterativeRobot {
+
+    /**
+     * Represents the drivetrain
+     */
+    public static DriveTrain driveTrain;
+
+    public static OI oi;
+
+    public static Robot robot;
+
+    public Robot() {
+    }
+
+    public void robotInit() {
+        robot = this;
+        // initialize subsystems
+        driveTrain = new DriveTrain();
+        oi = new OI();
+    }
 
 }
--- src/main/java/org/frc5687/steamworks/protobot/commands/DriveWith2Joysticks.java
+++ src/main/java/org/frc5687/steamworks/protobot/commands/DriveWith2Joysticks.java
@@ -1,13 +1,66 @@
 package org.frc5687.steamworks.protobot.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.frc5687.steamworks.protobot.OI;
+import static org.frc5687.steamworks.protobot.Robot.driveTrain;
+import static org.frc5687.steamworks.protobot.Robot.oi;
 
 /**
  * Created by Ben Bernard on 1/13/2017.
  */
 public class DriveWith2Joysticks extends Command {
+
+    /*
+     * Constructor
+     */
+    public DriveWith2Joysticks() {
+        requires(driveTrain);
+    }
+
+    /*
+     * Sets up the command
+     * Called just before this Command runs the first time(non-Javadoc)
+     * @see edu.wpi.first.wpilibj.command.Command#initialize()
+     */
+    protected void initialize() {
+        driveTrain.resetDriveEncoders();
+    }
+
+    /*
+     * Executes the command
+     * Called repeatedly when this Command is scheduled to run(non-Javadoc)
+     * @see edu.wpi.first.wpilibj.command.Command#execute()
+     */
+    protected void execute() {
+        driveTrain.tankDrive(oi.getLeftSpeed(), oi.getRightSpeed());
+    }
+
+    /*
+     * Check if this command is finished running
+     * Make this return true when this Command no longer needs to run execute()(non-Javadoc)
+     * @see edu.wpi.first.wpilibj.command.Command#isFinished()
+     */
     @Override
     protected boolean isFinished() {
         return false;
     }
+
+    /*
+     * Command execution clean-up
+     * Called once after isFinished returns true(non-Javadoc)
+     * @see edu.wpi.first.wpilibj.command.Command#end()
+     */
+    protected void end() {
+    }
+
+    /*
+     * Handler for when command is interrupted
+     * Called when another command which requires one or more of the same(non-Javadoc)
+     * @see edu.wpi.first.wpilibj.command.Command#interrupted()
+     */
+    protected void interrupted() {
+    }
+
 }
