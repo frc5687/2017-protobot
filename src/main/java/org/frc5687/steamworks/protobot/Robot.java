27d8326ffcc88f88b7544681589af2509cb85e8b
Ian Farm <ian.farm@baxter-academy.org>
2017-01-22 14:10:40 -0500

    Added camera streaming

--- src/main/java/org/frc5687/steamworks/protobot/Robot.java
+++ src/main/java/org/frc5687/steamworks/protobot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import org.frc5687.steamworks.protobot.subsystems.DriveTrain;
 import org.frc5687.steamworks.protobot.subsystems.GearHandler;
 import org.frc5687.steamworks.protobot.subsystems.Pneumatics;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.cscore.UsbCamera;
 
 /**
  * Created by Ben Bernard on 1/12/2017.
@@ -33,6 +35,8 @@ public class Robot extends IterativeRobot {
 
     public static Robot robot;
 
+    Thread visionThread;
+
     public Robot() {
     }
 
@@ -47,8 +51,10 @@ public class Robot extends IterativeRobot {
         driveTrain = new DriveTrain();
         gearHandler = new GearHandler();
         pneumatics = new Pneumatics();
+
         oi = new OI(); // must be initialized after subsystems
 
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
     }
 
     @Override
