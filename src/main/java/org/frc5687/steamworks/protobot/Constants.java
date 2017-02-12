6f02201f88fb188e5708ebc9d5fba8b264867b58
Caleb Marston <caleb.marston@baxter-academy.org>
2017-02-12 13:14:09 -0500

    Updates wheel diamteter to be accurate

--- src/main/java/org/frc5687/steamworks/protobot/Constants.java
+++ src/main/java/org/frc5687/steamworks/protobot/Constants.java
@@ -57,7 +57,7 @@ public class Constants {
             public static final boolean REVERSED = true; //TODO change to new robot specifications
             public static final int SAMPLES_TO_AVERAGE = 20;
             public static final int PULSES_PER_ROTATION = 1440;
-            public static final double WHEEL_DIAMETER = 6;
+            public static final double WHEEL_DIAMETER = 4;
             public static final double INCHES_PER_ROTATION = Math.PI * WHEEL_DIAMETER;
             public static final double SCALAR_RATIO = 8;
             public static final double INCHES_PER_PULSE = INCHES_PER_ROTATION * SCALAR_RATIO / PULSES_PER_ROTATION;
