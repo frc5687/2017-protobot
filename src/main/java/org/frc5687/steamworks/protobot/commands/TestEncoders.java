57cb858e69c02d974a8b5d833cd871b6e483ff14
Ian Farm <ian.farm@baxter-academy.org>
2017-02-15 15:04:50 -0500

    Formatting

--- src/main/java/org/frc5687/steamworks/protobot/commands/TestEncoders.java
+++ src/main/java/org/frc5687/steamworks/protobot/commands/TestEncoders.java
@@ -1,6 +1,5 @@
 package org.frc5687.steamworks.protobot.commands;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Command;
 import org.frc5687.steamworks.protobot.Constants;
 
@@ -10,16 +9,16 @@ import static org.frc5687.steamworks.protobot.Robot.driveTrain;
  * Created by Caleb on 2/13/2017.
  */
 public class TestEncoders extends Command {
-    public TestEncoders() {
-        requires(driveTrain);
 
-    }
-
-    private boolean IsEncoderWorking = false;
+    private boolean isEncoderWorking = false;
     private double startRightDistance = driveTrain.getRightDistance();
     private double startLeftDistance = driveTrain.getLeftDistance();
     private long startTime = System.currentTimeMillis();
     private long endTime = startTime + Constants.Drive.ENCODDER_TEST_TIME;
+
+    public TestEncoders() {
+        requires(driveTrain);
+    }
 
     @Override
     protected void initialize() {
@@ -32,15 +31,15 @@ public class TestEncoders extends Command {
         driveTrain.tankDrive(0,0);
         if (driveTrain.getLeftDistance()-startLeftDistance>Constants.Encoders.Threshholds.encodeThreshold ){
             if (driveTrain.getRightDistance()-startRightDistance> Constants.Encoders.Threshholds.encodeThreshold){
-                IsEncoderWorking = true;
+                isEncoderWorking = true;
             }
         }
-        //todo add an if statement that turns lights green if isEncoderWorkingand false otherwise
-
+        //TODO add an if statement that turns lights green if isEncoderWorking and false otherwise
     }
 
     @Override
     protected boolean isFinished() {
         return false;
     }
+
 }
