9c65ec8b76fb1ead8c15aa9fa71107d514261850
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-03-05 00:42:59 -0500

    Holding the mandibles open until the gear settles.

--- src/main/java/org/frc5687/steamworks/protobot/commands/autonomous/AutoDepositGear.java
+++ src/main/java/org/frc5687/steamworks/protobot/commands/autonomous/AutoDepositGear.java
@@ -1,6 +1,7 @@
 package org.frc5687.steamworks.protobot.commands.autonomous;
 
 import org.frc5687.steamworks.protobot.Constants;
+import org.frc5687.steamworks.protobot.commands.actions.HoldMandiblesOpen;
 import org.frc5687.steamworks.protobot.commands.composite.EjectMandibles;
 import org.frc5687.steamworks.protobot.commands.actions.AutoApproachTarget;
 import org.frc5687.steamworks.protobot.commands.actions.AutoDrive;
@@ -11,6 +12,8 @@ public class AutoDepositGear extends SteamworksBaseCommandGroup {
         super();
         addSequential(new AutoApproachTarget(Constants.Auto.Drive.SPEED));
         addSequential(new EjectMandibles());
+        addSequential(new HoldMandiblesOpen(500));
+        addParallel(new HoldMandiblesOpen(5000));
         addSequential(new AutoDrive(-Constants.Auto.AnglesAndDistances.RETREAT_DISTANCE, Constants.Auto.Drive.SPEED));
     }
 
