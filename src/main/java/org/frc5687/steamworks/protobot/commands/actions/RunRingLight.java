f8f79192e0fee3cad98188247ba6b068bcff63e1
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-03-05 00:41:39 -0500

    Adding manual ringlight control for testing.

---
+++ src/main/java/org/frc5687/steamworks/protobot/commands/actions/RunRingLight.java
@@ -0,0 +1,30 @@
+package org.frc5687.steamworks.protobot.commands.actions;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import static org.frc5687.steamworks.protobot.Robot.lights;
+
+/**
+ * Created by Ben Bernard on 3/4/2017.
+ */
+public class RunRingLight extends Command{
+
+    public RunRingLight() {
+        requires(lights);
+    }
+
+    @Override
+    protected void initialize() {
+        lights.turnRingLightOn();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        lights.turnRingLightOff();
+    }
+}
