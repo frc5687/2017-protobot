2746ecaf71758dc26d6718a1102f7c99b22bfbb0
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-03-06 23:36:43 -0500

    Holding mandibles open longer after gear deposit in auto.

--- src/main/java/org/frc5687/steamworks/protobot/commands/autonomous/AutoDepositRightFromFarRight.java
+++ src/main/java/org/frc5687/steamworks/protobot/commands/autonomous/AutoDepositRightFromFarRight.java
@@ -1,11 +1,8 @@
 package org.frc5687.steamworks.protobot.commands.autonomous;
 
 import org.frc5687.steamworks.protobot.Constants;
-import org.frc5687.steamworks.protobot.commands.actions.ReceiveMandibles;
+import org.frc5687.steamworks.protobot.commands.actions.*;
 import org.frc5687.steamworks.protobot.commands.composite.EjectMandibles;
-import org.frc5687.steamworks.protobot.commands.actions.AutoAlign;
-import org.frc5687.steamworks.protobot.commands.actions.AutoApproachTarget;
-import org.frc5687.steamworks.protobot.commands.actions.AutoDrive;
 
 /**
  * Created by Baxter on 2/25/2017.
@@ -19,6 +16,8 @@ public class AutoDepositRightFromFarRight extends SteamworksBaseCommandGroup {
         addSequential(new AutoAlign(-Constants.Auto.AnglesAndDistances.DEPOSIT_GEAR_FAR_ANGLE, Constants.Auto.Align.SPEED));
         addSequential(new AutoApproachTarget(Constants.Auto.Drive.SPEED));
         addSequential(new EjectMandibles());
+        addSequential(new HoldMandiblesOpen(500));
+        addParallel(new HoldMandiblesOpen(5000));
         addSequential(new AutoDrive(-Constants.Auto.AnglesAndDistances.RETREAT_DISTANCE, Constants.Auto.Drive.SPEED));
     }
 
