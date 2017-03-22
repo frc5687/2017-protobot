2dc22c3f0c46631655bcf39994acc9b668b83e4f
Caleb Marston <caleb.marston@baxter-academy.org>
2017-03-22 16:47:05 -0400

    adds test climber and mandibles to full test

--- src/main/java/org/frc5687/steamworks/protobot/commands/test/FullSelfTest.java
+++ src/main/java/org/frc5687/steamworks/protobot/commands/test/FullSelfTest.java
@@ -9,5 +9,8 @@ public class FullSelfTest extends CommandGroup {
     public FullSelfTest() {
         addSequential(new ConfirmTest("Please ensure the robot is drydocked and press Start to continue.", "Test started.", "Test aborted."));
         addSequential(new TestDriveTrain(1));
+        addSequential(new TestClimber(0.5));
+        addSequential(new TestMandibles(0.5));
+
     }
 }
