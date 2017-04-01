26532120f36c47c0fa8a9f6bcecbe84d57ab7924
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-04-01 13:46:26 -0400

    auto

--- src/main/java/org/frc5687/steamworks/protobot/commands/autonomous/AutoTraverseNeutralZoneFromSide.java
+++ src/main/java/org/frc5687/steamworks/protobot/commands/autonomous/AutoTraverseNeutralZoneFromSide.java
@@ -12,7 +12,7 @@ public class AutoTraverseNeutralZoneFromSide extends CommandGroup {
 
     public AutoTraverseNeutralZoneFromSide() {
         addSequential(new AutoAlign(Constants.Auto.AnglesAndDistances.STRAIGHT_ANGLE, Constants.Auto.Align.SPEED));
-        addSequential(new AutoDrive(Constants.Auto.AnglesAndDistances.TRAVERSE_NEUTRAL_ZONE_FROM_SIDE_DISTANCE, Constants.Auto.Drive.SPEED, 5000));
+        addSequential(new AutoDrive(Constants.Auto.AnglesAndDistances.TRAVERSE_NEUTRAL_ZONE_FROM_SIDE_DISTANCE, Constants.Auto.Drive.SPEED, 4000));
     }
 
 }
